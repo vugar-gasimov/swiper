065c7efc6b4ee1bc20049228747a56be5d4b6a5b
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-19 22:11:55 +0200

    Final desicion for svg mouse enter and mouse leave.

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { motion, useInView } from "framer-motion";
+
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
@@ -7,24 +7,13 @@ import "swiper/css/navigation";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
 import { ServiceData } from "../constants";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const Slider = () => {
-  const ref = useRef();
   const [showFullContent, setShowFullContent] = useState<
     Record<number, boolean>
   >({});
- 
-  const variant = {
-    initial: {
-      opacity: 0,
-    },
-    animate: {
-      opacity: 1,
-      transition: { duration: 1 },
-    },
-  };
- 
+  const [showSvg, setShowSvg] = useState<Record<number, boolean>>({});
 
   const toggleContent = (index: number) => {
     setShowFullContent((prev) => ({
@@ -32,7 +21,13 @@ const Slider = () => {
       [index]: !prev[index],
     }));
   };
-  const isInView = useInView(ref);
+  const toggleSvg = (index: number, isHovering: boolean) => {
+    setShowSvg((prev) => ({
+      ...prev,
+      [index]: isHovering,
+    }));
+  };
+
   return (
     <div className=" mx-auto px-4 md:px-8 py-16 bg-[#7A8868]">
       <h2 className="text-white font-bold text-[24px]">Популярне</h2>
@@ -54,31 +49,32 @@ const Slider = () => {
           prevEl: ".swiper-button-prev",
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
-        className="h-52rem pb-8 pt-16 relative"
+        className="h-52rem pb-8  relative"
       >
         {ServiceData.map((item, index) => (
           <SwiperSlide
             key={item.id}
-            className="flex justify-center"
-        
+            className="flex flex-col justify-center items-center gap-2"
           >
-            
-              <motion.div className="relative top-[-65px] right-[50%]  "     variants={variant}
-            initial="initial"
-            ref={ref}
-            animate={isInView && "animate"}>
+            {showSvg[index] && (
+              <div className=" ">
                 <img
                   className="w-[64px] h-[64px] z-10"
                   src={item.icon}
                   alt="Ukrainian Armed Forces emblems"
                 />
-              </motion.div>
+              </div>
             )}
-            <div className="flex flex-col justify-center  items-center gap-3 group relative shadow-lg  text-black rounded-xl p-5 max-h-[354px] w-[254px]  overflow-hidden cursor-pointer  bg-[#fff] overflow-y-auto ">
+
+            <div
+              className="flex flex-col justify-center  items-center gap-3 group relative shadow-lg  text-black rounded-xl p-5 max-h-[354px] w-[254px]  overflow-hidden cursor-pointer  bg-[#fff] overflow-y-auto "
+              onMouseEnter={() => toggleSvg(index, true)}
+              onMouseLeave={() => toggleSvg(index, false)}
+            >
               {!showFullContent[index] && (
                 <>
                   <div
-                    className="ablosute inset-0 bg-contain bg-no-repeat bg-center h-[200px] w-[134px]"
+                    className=" inset-0 bg-contain bg-no-repeat bg-center h-[200px] w-[134px]"
                     style={{
                       backgroundImage: `url(${item.backgroundImage})`,
                     }}
